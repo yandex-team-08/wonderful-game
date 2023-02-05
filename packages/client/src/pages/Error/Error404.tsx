03c970013f66f5de01099e27e84646327a07817c
innabatalova <batalovainna97@gmail.com>
2023-02-05 20:48:35 +0300

    Fix: repeat test part 4

--- packages/client/src/pages/Error/Error404.tsx
+++ packages/client/src/pages/Error/Error404.tsx
@@ -2,19 +2,19 @@ import { Typography } from '@mui/material';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import styles from './Error.module.scss';
-
 const Error404: FC = () => {
 
   const navigate = useNavigate();
 
   const onLogin = () => { navigate('/login'); };
 
-  return <div >
-    <Typography variant="h1">404</Typography>
-    <Typography variant="h2">Не туда попали...</Typography>
-    <Typography variant="h5" className={styles.link} onClick={onLogin}>назад в игру</Typography>
-  </div>;
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <Typography variant="h1" color="primary">404</Typography>
+      <Typography variant="h2" color="primary">Не туда попали...</Typography>
+      <Typography variant="h5" onClick={onLogin} color="error" sx={{ cursor: 'pointer' }}>назад в игру</Typography>
+    </div>
+  );
 };
 
 export default Error404;

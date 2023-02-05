f9aa682eb1d4059493d1573a49eaca0131cf29bf
innabatalova <batalovainna97@gmail.com>
2023-02-05 21:28:10 +0300

    Fix: resolve conflicts

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const BASE_API = 'https://ya-praktikum.tech/api/v2';
 
 export const AUTH_API = `${BASE_API}/auth`;
-export const USER_API = `${BASE_API}/user`;
+
+export const TEAM_NAME = 'team8';
